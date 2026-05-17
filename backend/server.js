60b65efc0f43b0f27a825d799a0c2e7c52fd0e51
caroline568 <caroline@DESKTOP-3N71K80.localdomain>
2026-05-17 13:35:28 -0400

    Add backend folder for Render deployment

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 5000;
+
+// MUST use this order
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port ${PORT}`);
+});
